2238a0afc06c0f183931a17123703e79f31ed561
Kim Se-Un <kseun1949@gmail.com>
2023-11-27 21:58:54 +0300

    fix: добавить в cors/origin https

--- utils/corsConfig.js
+++ utils/corsConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   origin: [
     'http://localhost:3000',
     'http://localhost:3001',
-    'kim.diplom.nomoredomainsmonster.ru',
+    'https://kim.diplom.nomoredomainsmonster.ru',
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'Accept'],
